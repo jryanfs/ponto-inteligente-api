e48a017ff1165d9b37ae1c5147af6aa4ec866e14
Jeferson Ryan <50426159+JefersonRyan@users.noreply.github.com>
2020-12-02 23:42:35 -0300

    Adicionando Classes

---
+++ src/main/java/com/kazale/pontointeligente/api/entities/Lancamento.java
@@ -0,0 +1,5 @@
+package com.kazale.pontointeligente.api.entities;
+
+public class Lancamento {
+    
+}
---
+++ src/main/java/com/kazale/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.kazale.pontointeligente.api.enums;
+
+public enum TipoEnum {
+    INICIO_TRABALHO,
+    TERMINO_TRABALHO,
+    INCIO_ALMOCO,
+    TERMINO_ALMOCO,
+    INICO_PAUSA,
+    TERMINO_PAUSA
+}
